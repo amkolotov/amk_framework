5a60c7763258fd8af9224720abc934aa0d0840a2
amkolotov <amkolotov@mail.ru>
2021-02-18 09:56:59 +0500

    commit_3

--- templates.py
+++ templates.py
@@ -1,4 +1,5 @@
-from jinja2 import Template
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 import os
 
 
@@ -8,7 +9,7 @@ def render(template_name, folder='templates', **kwargs):
     :param folder: имя папки с шаблонами
     :param kwargs: аргументы
     """
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding='utf-8') as file:
-        template = Template(file.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
